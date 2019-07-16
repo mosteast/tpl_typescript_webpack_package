16d37302b2d01bb12c4945d0f97a031b910f9baa
Hailang <thehailang@gmail.com>
2019-07-16 13:35:08 +0800

    Auto commit: 2019-07-16_13:35:08

--- build.script.ts
+++ build.script.ts
@@ -4,7 +4,9 @@ const shell = require('shelljs')
 
 const git_path = 'build/.git'
 const git_backup_path = './.git_tmp_backup'
-shell.exec(`mv ${git_path} ${git_backup_path}; rm -fr build/* && npx tsc && mv ${git_backup_path} ${git_path}`)
+shell.mv(git_path, git_backup_path)
+shell.exec(`rm -fr build/* && npx tsc`)
+shell.mv(git_backup_path, git_path)
 delete json.files
 json.main = 'index.js'
 json.type = 'index.d.ts'
