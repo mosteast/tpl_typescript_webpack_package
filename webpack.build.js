0bba60b5b97b0069e29f345664abffe58f54324d
kev <kev@localhost>
2020-01-13 17:53:26 +0800

    Auto commit: 2020-01-13_17:53:26

--- webpack.build.js
+++ webpack.build.js
@@ -1,14 +1,14 @@
-const Clean = require('clean-webpack-plugin');
-const merge = require('webpack-merge');
+const Clean = require('clean-webpack-plugin')
+const merge = require('webpack-merge')
 
 module.exports = merge(require('./webpack.common'), {
-  mode    : process.env.NODE_ENV || 'production',
-  output  : {
-    filename : "[hash].bundle.js",
-    path     : __dirname + "/dist",
-  },
+	mode: 'production',
+	output: {
+		filename: '[hash].bundle.js',
+		path: __dirname + '/build',
+	},
 
-  plugins : [
-    new Clean([ './dist' ]),
-  ],
-});
+	plugins: [
+		new Clean(['./build']),
+	],
+})
