df539d896c48f126f9f9a7d923084cf2fc2a3f8e
kev <kev@localhost>
2020-08-03 18:19:13 +0800

    Auto commit: 2020-08-03_18:19:13

---
+++ src/declaration.d.ts
@@ -0,0 +1,11 @@
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+declare module '*.woff';
+declare module '*.woff2';
+declare module '*.ttf';
+declare module '*.eot';
+declare module '*.png';
+declare module '*.webm';
+declare module '*.mp4';
